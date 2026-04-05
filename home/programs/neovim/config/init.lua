a2ec27724d70424d9b3af106df32dc3090a1b6c5
mitsiu-carreno <kincaid@kincaids-MacBook-Air.local>
2026-04-05 08:30:09 -0600

    random nvim theme

--- home/programs/neovim/config/init.lua
+++ home/programs/neovim/config/init.lua
@@ -1,2 +1,37 @@
 require("options")
 require("keymaps")
+
+local light_themes = {
+  'mfd', 'mfd-paper', 'mfd-flir-bh', 'mfd-gbl-light',
+};
+
+local dark_themes = {
+  'mfd-dark', 'mfd-stealth', 'mfd-amber', 'mfd-mono',
+  'mfd-scarlet', 'mfd-hud', 'mfd-nvg', 'mfd-blackout',
+  'mfd-flir', 'mfd-flir-rh', 'mfd-flir-fusion', 'mfd-gbl-dark', 'mfd-lumon'
+}
+
+local themes = {}
+
+vim.list_extend(themes, dark_themes)
+-- vim.list_extend(themes, light_themes)
+
+math.randomseed(os.time())
+local chosen = themes[math.random(#themes)]
+
+require('mfd').setup({
+  bright_comments = true,  -- helps readability due to transparency
+});
+
+vim.cmd('colorscheme ' .. chosen)
+
+vim.opt.guicursor = {
+  "n:block-CursorNormal",
+  "v:block-CursorVisual",
+  "i:block-CursorInsert",
+  "r-cr:block-CursorReplace",
+  "c:block-CursorCommand",
+}
+require('mfd').enable_cursor_sync()
+
+vim.notify('mfd: ' .. chosen, vim.log.levels.INFO)
--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -1,8 +1,24 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  mfd-nvim = pkgs.vimUtils.buildVimPlugin {
+    name = "mfd.nvim";
+    src = pkgs.fetchFromGitHub {
+      owner = "kungfusheep";
+      repo = "mfd.nvim";
+      rev = "master";
+      sha256 = "sha256-MXarF5J+r654B6W+AzIzNgvWzCaYfTPWQnVfPmMfcA8=";
+    };
+  };
+in
+{
 
 	programs.neovim = {
 		enable = true;
 		defaultEditor = true;
+
+    plugins = [
+      mfd-nvim
+    ];
 	};
 
 	xdg.configFile."nvim/init.lua".source = ./config/init.lua;
