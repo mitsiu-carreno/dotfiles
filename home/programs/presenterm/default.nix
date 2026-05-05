1af0b09fe77d73444c232e4c6c1c38e5877ae526
mitsiu-carreno <mitsiu.carreno@gmail.com>
2026-05-04 19:27:26 -0600

    add presenterm highlighters

--- home/programs/presenterm/default.nix
+++ home/programs/presenterm/default.nix
@@ -7,8 +7,8 @@
 
   xdg.configFile."presenterm/config.yaml".source = ./config/config.yaml;
   xdg.configFile."presenterm/themes/custom-dark.yaml".source = ./config/themes/custom-dark.yaml;
-  #xdg.configFile."presenterm/themes/highlighting/Dracula.tmTheme".source = ./config/themes/highlighting/Dracula.tmTheme;
+  xdg.configFile."presenterm/themes/highlighting/Dracula.tmTheme".source = ./config/themes/highlighting/Dracula.tmTheme;
   xdg.configFile."presenterm/themes/highlighting/Nord.tmTheme".source = ./config/themes/highlighting/Nord.tmTheme;
-  xdg.configFile."presenterm/themes/highlighting/Monokai_Bright.tmTheme".source = ./config/themes/highlighting/Monokai_Bright.tmTheme;
+  xdg.configFile."presenterm/themes/highlighting/Monokai.tmTheme".source = ./config/themes/highlighting/Monokai.tmTheme;
 
 }
